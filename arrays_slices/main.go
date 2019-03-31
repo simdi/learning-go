3f6dda85ebb16a89e0a266e3d6bdacc2ce7f7b17
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-03-31 14:34:21 +0100

    Updated.

---
+++ arrays_slices/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Arrays
+	// var fruitArr [2]string
+
+	// Assign values
+	// fruitArr[0] = "Orange"
+	// fruitArr[1] = "mango"
+
+	fruitArr := []string{"Apple", "Orange", "Mango"}
+
+	fmt.Println(fruitArr)
+}
---
+++ maps/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Define maps
+	emails := make(map[string]string)
+
+	// Assign key values
+	emails["simdi"] = "simdi@yahoo.com"
+	emails["john"] = "john@yahoo.com"
+
+	fmt.Println(emails)
+}
