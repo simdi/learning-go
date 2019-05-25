fb9fadc8471eec186666a1c69afde782cbd084f1
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-05-25 14:54:42 +0100

    Done with stack.

---
+++ stacker/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/simdi/firstproject/stacker/stack"
+)
+
+func main() {
+	var haystack stack.Stack
+	haystack.Push("hay")
+	haystack.Push(-15)
+	haystack.Push([]string{"pin", "clip", "needle"})
+	haystack.Push(81.52)
+	for {
+		item, err := haystack.Pop()
+		if err != nil {
+			break
+		}
+		fmt.Println(item)
+	}
+}
