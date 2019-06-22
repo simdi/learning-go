87610732a25d519f92a3c58e2f4d2ba5245eef0e
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-06-22 23:23:32 +0100

    Still working on americanise.

---
+++ americanise/main.go
@@ -0,0 +1,79 @@
+import ( "bufio"
+    "fmt"
+    "io"
+    "io/ioutil"
+    "log"
+    "os"
+    "path/filepath"
+    "regexp"
+    "strings"
+)
+
+func main() {
+	inFilename, outFilename, err := filenamesFromCommandLin()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	inFile, outFile := os.Stdin, os.Stdout
+	if inFilename != "" {
+		if inFile, err = os.Open(inFilename); err != nil { 			log.Fatal(err)
+		}
+		defer inFile.Close()
+	}
+	if outFilename != "" {
+		if outFile, err = os.Create(outFilename); err != nil {
+			log.Fatal(err)
+		}
+		defer outFile.Close()
+	}
+	if err = americanise(inFile, outFile); err != nil { 			log.Fatal(err)
+	}
+}
+
+
+func filenamesFromCommandLine() (inFilename, outFilename string, err error) {
+	if len(os.Args) > 1 && (os.Args[1] == "-h" || os.Args[1] == "--help") { 
+		err = fmt.Errorf("usage: %s [<]infile.txt [>]outfile.txt", filepath.Base(os.Args[0]))
+		return "", "", err
+	}
+	if len(os.Args) > 1 {
+		inFilename = os.Args[1]
+		if len(os.Args) > 2 {
+			outFilename = os.Args[2]
+		}
+	}
+	if inFilename != "" && inFilename == outFilename {
+		log.Fatal("won't overwrite the infile")
+	}
+	return inFilename, outFilename, nil
+}
+
+var britishAmerican = "british-american.txt"
+func americanise(inFile io.Reader, outFile io.Writer) (err error) {
+	reader := bufio.NewReader(inFile)
+	writer := bufio.NewWriter(outFile)
+	defer func() {
+		if err == nil {
+			err = writer.Flush()
+		}
+	}()
+	var replacer
+	func(string) string
+		if replacer, err = makeReplacerFunction(britishAmerican); err != nil {
+		return err
+	}
+	wordRx := regexp.MustCompile("[A-Za-z]+") eof := false
+	for !eof {
+		var line string
+		line, err = reader.ReadString('\n') if err == io.EOF {
+		err = nil // io.EOF isn't really an error
+		eof = true // this will end the loop at the next iteration } else if err != nil {
+		return err // finish immediately for real errors }
+		line = wordRx.ReplaceAllStringFunc(line, replacer)
+		if _, err = writer.WriteString(line); err != nil {
+			return err
+		}
+	}
+	return nil
+}
