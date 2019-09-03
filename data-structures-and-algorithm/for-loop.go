2430c537617c4f96f5f57ad41fe57cf1d7340fc2
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-09-03 10:22:44 +0100

    finished drawing a rectangle with a for loop.

---
+++ data-structures-and-algorithm/for-loop.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/*
+	* Write a program to print out
+	*	G
+	*	GG
+	*	GGG
+	* GGGG
+	*	...
+	 */
+	for i := 0; i < 25; i++ {
+		for j := 0; j <= i; j++ {
+			fmt.Print("G")
+		}
+		fmt.Print("\n")
+	}
+
+	/*
+	* Write a program to print out
+	*	GGGGGGGGGGGGGGG
+	*	G             G
+	*	G             G
+	*	G             G
+	*	G             G
+	*	GGGGGGGGGGGGGGG
+	 */
+	for i := 0; i < 10; i++ {
+		for j := 0; j <= 23; j++ {
+			if i == 0 || i == 9 || j == 0 || j == 23 {
+				fmt.Print("G")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Print("\n")
+	}
+}
