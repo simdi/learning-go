2e3f85f54c4c28cd5d9da0b354735c8518e03bd9
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-09-05 06:22:14 +0100

    Still working on for looping.

--- data-structures-and-algorithm/for-loop.go
+++ data-structures-and-algorithm/for-loop.go
@@ -13,12 +13,12 @@ func main() {
 	* GGGG
 	*	...
 	 */
-	for i := 0; i < 25; i++ {
-		for j := 0; j <= i; j++ {
-			fmt.Print("G")
-		}
-		fmt.Print("\n")
-	}
+	// for i := 0; i < 25; i++ {
+	// 	for j := 0; j <= i; j++ {
+	// 		fmt.Print("G")
+	// 	}
+	// 	fmt.Print("\n")
+	// }
 
 	/*
 	* Write a program to print out
@@ -29,9 +29,31 @@ func main() {
 	*	G             G
 	*	GGGGGGGGGGGGGGG
 	 */
+	// for i := 0; i < 10; i++ {
+	// 	for j := 0; j <= 23; j++ {
+	// 		if i == 0 || i == 9 || j == 0 || j == 23 {
+	// 			fmt.Print("G")
+	// 		} else {
+	// 			fmt.Print(" ")
+	// 		}
+	// 	}
+	// 	fmt.Print("\n")
+	// }
+	/*
+	* Write a program to print out
+	*	G
+	*	GG
+	*	GGG
+	*	GGGG
+	*	GGGGG
+	*	GGGG
+	*	GGG
+	*	GG
+	*	G
+	 */
 	for i := 0; i < 10; i++ {
-		for j := 0; j <= 23; j++ {
-			if i == 0 || i == 9 || j == 0 || j == 23 {
+		for j := 0; j < 10; j++ {
+			if i > j {
 				fmt.Print("G")
 			} else {
 				fmt.Print(" ")
