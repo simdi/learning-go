3e54742fd7f4e2ddc9ed446df7fe41bb251255e6
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-11-09 23:06:35 +0100

    Updated time.

--- date-and-time/main.go
+++ date-and-time/main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
+	t := time.Now()
+	// t := "2019-10-03T06:31:01.998Z"
+	fmt.Println("Time", t)
+	fmt.Println(t.Format(time.ANSIC))
 	myDateString := "2018-01-20"
 	formatDate, err := time.Parse("2006-01-02", myDateString)
 	if err != nil {
